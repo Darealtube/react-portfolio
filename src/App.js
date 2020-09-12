c31e0db374fc12005e0b0f7f93648a98ff33481e
Darealtube <evajavier143@gmail.com>
2020-09-12 15:11:06 +0800

    "First Commit"

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faGrinStars} from '@fortawesome/free-regular-svg-icons'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {abouts} from './texts/texts.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+    <div id="wrap">
+    <div className="first">
+    <div className="bg"></div>
+      <span className="intro">
+      <FontAwesomeIcon id="grin" icon={faGrinStars}/> <br/><br/>
+      <h1>Hey! Welcome.</h1>
+      <h2>This is my portfolio, created with React, HTML, and CSS.</h2>
+      </span>
+    </div>
+
+    <div className="second">
+    <div className="bg2"></div>
+      <span className="about">
+
+      <img src={require('./images/darryl2.png')} width="180" height="180"/>
+      <br/><br/>
+      <h1>About Me:</h1>
+      <p>{abouts}</p>
+      </span>
+    </div>
+    </div>
+
     </div>
   );
 }
---
+++ src/texts/texts.js
@@ -0,0 +1,3 @@
+export var abouts = `I am Darryl B. Javier. At the time of writing, I am a HS student at Pasig City Science
+              Highschool in the Philippines. I pursue Web Development because I like it, and I want
+              to do something for everyone through the use of web services. Nice to meet you!`;
