fec6b1cc1a18a2e824a4983b35c39fd981717038
Darealtube <evajavier143@gmail.com>
2020-09-22 17:59:41 +0800

    Updates

---
+++ precache-manifest.cccc07d7209537a6cce1dc5a2d063e18.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcafb4494a6b2bfc633bc3cb493b0d45",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "88359626a4ff16e1af9e",
+    "url": "/react-portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "9f9d35ce1bfbc3e2aa85",
+    "url": "/react-portfolio/static/css/main.3e618c7b.chunk.css"
+  },
+  {
+    "revision": "88359626a4ff16e1af9e",
+    "url": "/react-portfolio/static/js/2.44875e99.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.44875e99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f9d35ce1bfbc3e2aa85",
+    "url": "/react-portfolio/static/js/main.59d8bf7b.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "c2c09e8aa50e73d7dd61798b46402a89",
+    "url": "/react-portfolio/static/media/Calculator.c2c09e8a.png"
+  },
+  {
+    "revision": "8687e8f1056e570d9bf76062bf3e691d",
+    "url": "/react-portfolio/static/media/Pomodoro.8687e8f1.png"
+  },
+  {
+    "revision": "f152a47c79cc94afbc999a6094e04775",
+    "url": "/react-portfolio/static/media/back2.f152a47c.jpg"
+  },
+  {
+    "revision": "da8bb1d6b15181719a2353f659b4346c",
+    "url": "/react-portfolio/static/media/code.da8bb1d6.jpg"
+  },
+  {
+    "revision": "5eb8c665833d7115c0a646faf37c658b",
+    "url": "/react-portfolio/static/media/darryl2.5eb8c665.png"
+  },
+  {
+    "revision": "fc68804045c0f9068ae1d9a724324f0b",
+    "url": "/react-portfolio/static/media/dr.fc688040.png"
+  },
+  {
+    "revision": "e544924579a9196716fa479e24930c2b",
+    "url": "/react-portfolio/static/media/im1.e5449245.png"
+  },
+  {
+    "revision": "a4180693e55f1eeddfd1e05a4dabeac2",
+    "url": "/react-portfolio/static/media/mar2.a4180693.png"
+  },
+  {
+    "revision": "9fb5782aff482a97ff5107f1b0cb2610",
+    "url": "/react-portfolio/static/media/reactimage.9fb5782a.jpeg"
+  },
+  {
+    "revision": "28bc2318a2d2ca002d7ab1f745d11bbf",
+    "url": "/react-portfolio/static/media/this.28bc2318.png"
+  }
+]);
